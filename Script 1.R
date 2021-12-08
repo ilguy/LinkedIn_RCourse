6b43a55ceed085486d43e5657a36538ea09b5779
ilguy <yostr01@yahoo.com>
2021-12-07 19:04:08 -0600

    Sample datasets

--- Script 1.R
+++ Script 1.R
@@ -1,5 +1,29 @@
-#### Import data 
+# Import data =====
 
 library(readr)
 StateData <- read_csv("C:/Users/ryost/OneDrive/R/Ex_Files_Learning_R/Exercise Files/data/StateData.csv")
 View(StateData)
+
+# Load Packages ====
+library(datasets)
+
+## Iris data =====
+?iris
+iris
+
+## UCBAdmissions ====
+?UCBAdmissions
+UCBAdmissions
+
+## Titanic ====
+?Titanic
+Titanic
+
+## state.x77 ====
+?state.x77
+state.x77
+
+## swiss ====
+?swiss
+swiss
+
